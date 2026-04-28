02ee07a18c1d007edddce975a8922486ce93ed05
Rudendru-03 <rahul.squaregroup.tech>
2026-04-28 16:35:14 +0530

    name of redis url changed

--- services/redis.js
+++ services/redis.js
@@ -1,5 +1,5 @@
 const redisConnection = {
-  host: process.env.REDIS_HOST || "127.0.0.1",
+  host: process.env.REDIS_URL || "127.0.0.1",
   port: Number(process.env.REDIS_PORT || 6379),
   password: process.env.REDIS_PASSWORD || undefined,
   db: Number(process.env.REDIS_DB || 0),
